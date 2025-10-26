261787617d908e32dd1d2cffc5d63e36defbba5d
RudrakshTiwari08 <rudrakshat825@gmail.com>
2025-10-26 13:36:10 +0530

    Add files via upload

---
+++ beautiful_year.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int nextDistinctYear(int y)
+    {
+        y++;
+        while (true)
+        {
+            int a = y / 1000;
+            int b = (y / 100) % 10;
+            int c = (y / 10) % 10;
+            int d = y % 10;
+            if (a != b && a != c && a != d &&
+                b != c && b != d &&
+                c != d)
+                return y;
+
+            y++;
+        }
+    }
+};
+
+int main()
+{
+    int y;
+    cin >> y;
+
+    Solution sol;
+    cout << sol.nextDistinctYear(y) << endl;
+
+    return 0;
+}
